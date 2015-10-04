f3b301dccb55515b3b56f7d60a3f55d03ce172d3
Albert Garcia <albert.garcia@obokaman.com>
2015-10-04 16:25:39 +0200

    Remove invalid test for Week period

--- tests/Period/WeekTest.php
+++ tests/Period/WeekTest.php
@@ -7,13 +7,6 @@ use DateRanger\Period\Week;
 final class WeekTest extends \PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function validWeek()
-    {
-        $week = new Week();
-        $this->assertEquals(date('Y-m-W'), $week->start()->format('Y-m-W'));
-    }
-
-    /** @test */
     public function getCorrectFirstDayWeek()
     {
         $week = new Week('2015-01-01');
